a98043f821f8e9c23644c8b41c2f9702c0037bba
Sibel <sbaslamisli@yahoo.com>
2021-03-08 01:50:46 -0500

    chef`s page-image slide part

--- app.js
+++ app.js
@@ -136,3 +136,22 @@ function throttle(func, limit) {
 }
 
 init();
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slideshow = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slideshow.length; i++) {
+        slideshow[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slideshow.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideshow[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
