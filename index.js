f0ce2a482102769e5123c67c7e8b3aeeb75191b5
Sibel <sbaslamisli@yahoo.com>
2021-03-22 19:39:11 -0400

    form with validation

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
-//Contact page- form submit
-const handle_form_submission = () => {
-    alert('Message sent it.');
-}
+// //AOS animation call
+// AOS.init({
+//     duration: 1200,
+// })
 
-//AOS animation call
-AOS.init({
-    duration: 1200,
-})
+// //change the font size
+// $("#fader").on("input", function() {
+//     $('#content').css("font-size", $(this).val() + "px");
+// });
 
-//change the font size
-$("#fader").on("input", function() {
-    $('#content').css("font-size", $(this).val() + "px");
-});
+// //chef navbar-background color change when the page is active
+// function setActive() {
+//     aObj = document.getElementById('sidenav').getElementsByClassName('navlink');
+//     for (i = 0; i < aObj.length; i++) {
+//         if (document.location.href.indexOf(aObj[i].href) >= 0) {
+//             aObj[i].className = 'active';
+//         }
+//     }
+// }
+// window.onload = setActive();
 
-//chef navbar-background color change when the page is active
-function setActive() {
-    aObj = document.getElementById('sidenav').getElementsByClassName('navlink');
-    for (i = 0; i < aObj.length; i++) {
-        if (document.location.href.indexOf(aObj[i].href) >= 0) {
-            aObj[i].className = 'active';
-        }
+
+
+const validate = () => {
+    let name = document.getElementById("name").value;
+    let subject = document.getElementById("subject").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    let error_message = document.getElementById("error_message");
+
+    error_message.style.padding = "10px";
+
+    let text;
+    if (name.length < 5) {
+        text = "Please Enter valid Name";
+        error_message.innerHTML = text;
+        return false;
     }
+    if (subject.length < 2) {
+        text = "Please Enter Correct Subject";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if (isNaN(phone) || phone.length != 10) {
+        text = "Please Enter valid Phone Number";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if (email.indexOf("@") == -1 || email.length < 6) {
+        text = "Please Enter valid Email";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if (message.length <= 20) {
+        text = "Please Enter More Than 40 Characters";
+        error_message.innerHTML = text;
+        return false;
+    }
+    alert("Form Submitted Successfully!");
+    return true;
 }
-window.onload = setActive();
