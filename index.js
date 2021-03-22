8e58b759c4561feaf78d88508d8ede9287dc9bf3
Sibel <sbaslamisli@yahoo.com>
2021-03-22 19:47:20 -0400

    index.js update

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// //AOS animation call
-// AOS.init({
-//     duration: 1200,
-// })
+//AOS animation call
+AOS.init({
+    duration: 1200,
+})
 
-// //change the font size
-// $("#fader").on("input", function() {
-//     $('#content').css("font-size", $(this).val() + "px");
-// });
+//change the font size
+$("#fader").on("input", function() {
+    $('#content').css("font-size", $(this).val() + "px");
+});
 
-// //chef navbar-background color change when the page is active
-// function setActive() {
-//     aObj = document.getElementById('sidenav').getElementsByClassName('navlink');
-//     for (i = 0; i < aObj.length; i++) {
-//         if (document.location.href.indexOf(aObj[i].href) >= 0) {
-//             aObj[i].className = 'active';
-//         }
-//     }
-// }
-// window.onload = setActive();
+//chef navbar-background color change when the page is active
+function setActive() {
+    aObj = document.getElementById('sidenav').getElementsByClassName('navlink');
+    for (i = 0; i < aObj.length; i++) {
+        if (document.location.href.indexOf(aObj[i].href) >= 0) {
+            aObj[i].className = 'active';
+        }
+    }
+}
+window.onload = setActive();
 
 
 
